6b05e6c5c7a13cf510f4a3127426f289a286f412
zeeshanmehmood59640 <r0959598@student.thomasmore.be>
2024-08-17 20:50:29 +0200

    t deploy

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,34 @@
 */
 //
 // Scripts
-// 
+//
+const wrapper = document.getElementById("test");
+const frameContainer = wrapper.querySelector(".container-st");
+const framesAll = frameContainer.querySelectorAll(".frame");
+const framesNum = framesAll.length;
+for (let i = 0; i < framesNum; i++) {
+    framesAll[i].style.order = i + 1;
+}
+const scrollClick = (direction) => {
+    for (let i = 0; i < framesNum; i++) {
+        let current_order = Number(framesAll[i].style.order);
+        if (direction == "forward") {
+            if (current_order == 1) {
+                var new_order = framesNum;
+            } else {
+                var new_order = Number(current_order) - 1;
+            }
+        } else if (direction == "backward") {
+            if (current_order == framesNum) {
+                var new_order = 1;
+            } else {
+                var new_order = Number(current_order) + 1;
+            }
+        }
+        framesAll[i].style.order = new_order;
+    }
+};
+
 
 window.addEventListener('DOMContentLoaded', event => {
 
